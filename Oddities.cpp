fea8008403d5e3da2024ff6d5c76c8252f5483c1
MuhammadMagdy98 <52232260+MuhammadMagdy98@users.noreply.github.com>
2019-12-03 10:53:30 +0200

    Create Oddities.cpp

---
+++ Oddities.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  while (n--) {
+    int x;
+    cin >> x;
+    if (x % 2 == 0) {
+      cout << x << " is even\n";
+    } else {
+      cout << x << " is odd\n";
+    }
+  }
+  return 0;
+}
