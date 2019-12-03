6cc097b9b2994980d453fad17d55fec7e380362e
MuhammadMagdy98 <52232260+MuhammadMagdy98@users.noreply.github.com>
2019-12-03 11:10:00 +0200

    Create Hissing Microphone.cpp

---
+++ Hissing Microphone.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  if (s.find("ss") != string::npos) {
+    cout << "hiss\n";
+  } else {
+    cout << "no hiss\n";
+  }
+  return 0;
+}
